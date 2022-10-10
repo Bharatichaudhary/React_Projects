489690edf4f9f51e530043eb04175dd528911dc7
Bharatichaudhary <bharatichaudhary85@gmail.com>
2022-10-10 13:21:36 +0545

    Project in react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Game from './component/game';
+//import Question from './component/Question';
+//import ToDoList from './component/to_do_list';
+//import Temperature from './component/temperature';
+//import Temperature from "./component/temperature"
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Temperature/>*/}
+     {/*<ToDoList/>*/}
+     {/*<Question/>*/}
+     <Game/>
+
+    </div>
+  );
+}
+
+export default App;
